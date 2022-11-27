85aa2ae14d82959929de350bf7930e762d9c88c5
MakGris <maxgrishenko@gmail.com>
2022-11-27 16:44:37 +0700

    working with NetworkService

--- VkNewsFeed/Services/NetworkService.swift
+++ VkNewsFeed/Services/NetworkService.swift
@@ -7,27 +7,55 @@
 
 import Foundation
 
-class NetworkService {
+protocol Networking {
+    func request(path: String, parameters: [String: String], completion: @escaping (Data?, Error?) -> Void)
+}
+
+class NetworkService: Networking {
+    
     private let authService: AuthService
     
     init(authService: AuthService = SceneDelegate.shared().authService) {
         self.authService = authService
     }
+    
+    func request(path: String, parameters: [String : String], completion: @escaping (Data?, Error?) -> Void) {
+        guard let token = authService.token else { return }
+        var allParameters = parameters
+        allParameters["access_token"] = token
+        allParameters["v"] = API.version
+        let url = url(from: path, parameters: allParameters)
+        let request = URLRequest(url: url)
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
+        print(url)
+    }
+    
     func getFeed() {
-        
-        var components = URLComponents()
         guard let token = authService.token else { return }
         let parameters = ["filters": "post, photo"]
         var allParameters = parameters
         allParameters["access_token"] = token
         allParameters["v"] = API.version
-        
+        let url = url(from: API.newsfeed, parameters: allParameters)
+        print(url)
+    }
+    
+    private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        })
+    }
+    
+    private func url(from path: String, parameters: [String: String]) -> URL {
+        var components = URLComponents()
         components.scheme = API.scheme
         components.host = API.host
         components.path = API.newsfeed
-        components.queryItems = allParameters.map{ URLQueryItem(name: $0, value: $1) }
+        components.queryItems = parameters.map{ URLQueryItem(name: $0, value: $1) }
         
-       let url = components.url!
-        print(url)
+       return components.url!
     }
 }
